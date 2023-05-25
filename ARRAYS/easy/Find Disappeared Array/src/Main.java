95afb4a207e48e4752be27a31ae5bdbfb9691151
Ahmed-Camara <camarahmed123@gmail.com>
2023-05-25 15:28:48 +0100

    448. Find All Numbers Disappeared in an Array

---
+++ ARRAYS/easy/Find Disappeared Array/src/Main.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] nums1 = new int[8];
+		nums1[0] = 4;
+		nums1[1] = 3;
+		nums1[2] = 2;
+		nums1[3] = 7;
+		nums1[4] = 8;
+		nums1[5] = 2;
+		nums1[6] = 3;
+		nums1[7] = 1;
+		
+		int[] nums2 = new int[2];
+		nums2[0] = 1;
+		nums2[1] = 1;
+		
+		List<Integer> results = findDisappearedNumbers(nums1);
+		
+		for(int inter : results) {
+			System.out.print(inter+" ");
+		}
+	}
+	
+	public static List<Integer> findDisappearedNumbers(int[] nums) {
+		
+		Set<Integer> results = new HashSet<>();
+		
+		
+		for(int i = 1; i <= nums.length; i++) {
+			results.add(i);
+		}
+		
+		for(int i = 0; i < nums.length; i++) {
+			if(results.contains(nums[i]) ){				
+				results.remove((Integer)nums[i]);
+			}
+		}
+		
+		
+		return new ArrayList<>(results);
+	}
+}
---
+++ ARRAYS/easy/Intersection of Two Arrays II/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] nums1 = new int[4];
+		nums1[0] = 1;
+		nums1[1] = 2;
+		nums1[2] = 2;
+		nums1[3] = 1;
+		
+		int[] nums2 = new int[2];
+		nums2[0] = 2;
+		nums2[1] = 2;
+		
+		
+		int[] nums3 = new int[3];
+		nums3[0] = 4;
+		nums3[1] = 9;
+		nums3[2] = 5;
+		
+		int[] nums4 = new int[5];
+		nums4[0] = 9;
+		nums4[1] = 4;
+		nums4[2] = 9;
+		nums4[3] = 8;
+		nums4[4] = 4;
+		
+		
+		int[] intersection = intersection(nums1, nums2);
+		
+		for(int inter : intersection) {
+			System.out.print(inter+" ");
+		}
+	}
+	
+	public static int[] intersection(int[] nums1, int[] nums2) {
+		
+		List<Integer> firstList = new ArrayList<>();
+		List<Integer> secondList = new ArrayList<>();
+		List<Integer> results = new ArrayList<>();
+		
+		for(int i = 0; i < nums1.length; i++)
+			firstList.add(nums1[i]);
+		
+		
+		for(int i = 0; i < nums2.length; i++) {
+			
+			if(firstList.contains(nums2[i]) && !results.contains(nums2[i])) {
+				results.add(nums2[i]);
+			}
+		}
+		
+		int []intersection = new int[results.size()];
+		
+		Iterator<Integer> iterator = results.iterator();
+		int k = 0;
+        while (iterator.hasNext()) {
+        	intersection[k] = iterator.next();
+        	k++;
+        }
+		
+		return intersection;
+	}
+}
