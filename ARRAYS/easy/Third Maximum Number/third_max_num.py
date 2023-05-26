1caffecf89259a52bc5aab25bb517f4c8a4fc75f
Ahmed-Camara <camarahmed123@gmail.com>
2023-05-26 20:37:15 +0100

    44. Third Maximum Number

--- ARRAYS/easy/Third Maximum Number/third_max_num.py
+++ ARRAYS/easy/Third Maximum Number/third_max_num.py
@@ -1,5 +1,21 @@
-nums = [3, 2, 1]
+num = [3, 2,2, 1]
 
 
 def thirdMax(nums):
-    print("Hello")
+    max,secondMax,thirdMax = float('-inf'),float('-inf'),float('-inf')
+
+    for num in nums:
+        if num > max:
+            thirdMax = secondMax
+            secondMax = max
+            max = num
+        elif num > secondMax and num < max:
+            thirdMax = secondMax
+            secondMax = num
+
+        elif num > thirdMax and num < secondMax:
+            thirdMax = num
+
+    result = max if thirdMax == float('-inf') else thirdMax
+    return result
+print(thirdMax(num))
