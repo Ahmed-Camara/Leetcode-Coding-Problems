5724ce85c7a36e7c816300a1d2f39b72b371657b
Ahmed-Camara <camarahmed123@gmail.com>
2023-05-30 15:31:26 +0100

    485. Max Consecutive Ones

---
+++ ARRAYS/easy/Max Consecutive Ones/MaxConsecutiveOnes.py
@@ -0,0 +1,10 @@
+def findMaxConsecutiveOnes(nums):
+    count = 0
+    return count
+
+
+num = [1,1,0,1,1,1]
+print(findMaxConsecutiveOnes(num))
+
+num = [1,0,1,1,0,1]
+print(findMaxConsecutiveOnes(num))
