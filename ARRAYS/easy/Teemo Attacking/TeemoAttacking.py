a0a173dea43025eb1cb9ed311ead34f68b564309
Ahmed-Camara <camarahmed123@gmail.com>
2023-05-31 10:54:34 +0100

    495. Teemo Attacking

--- ARRAYS/easy/Teemo Attacking/TeemoAttacking.py
+++ ARRAYS/easy/Teemo Attacking/TeemoAttacking.py
@@ -3,7 +3,6 @@ class Solution(object):
     def findPoisonedDuration(self,timeSeries, duration):
     
         total = 0
-
         for i in range(len(timeSeries)-1):
             total = total + min((timeSeries[i+1]-timeSeries[i]), duration)
         return total + duration
