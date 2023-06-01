1e615f41de033fc77eb64f12ec4f3e0511eaf4c8
Ahmed-Camara <camarahmed123@gmail.com>
2023-06-01 17:40:13 +0100

    496. Next Greater Element I

--- ARRAYS/easy/Max Consecutive Ones/NextGreaterElement_I.py
+++ ARRAYS/easy/Max Consecutive Ones/NextGreaterElement_I.py
@@ -5,3 +5,24 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        hashIndex = {n:i for i, n in enumerate(nums1)}
+        elements = [-1] * len(nums1)
+        
+        for i in range(len(nums2)):
+
+            if nums2[i] in hashIndex:
+                for j in range(i + 1,len(nums2)):
+                    if nums2[j] > nums2[i]:
+                        ids = hashIndex[nums2[i]]
+                        elements[ids] = nums2[j]
+                        break
+                    
+
+        
+        return elements
+    
+print(Solution().nextGreaterElement([4,1,2],[1,3,4,2]))
+
+print(Solution().nextGreaterElement([2,4],[1,2,3,4]))
+
+print(Solution().nextGreaterElement([1,3,5,2,4],[6,5,4,3,2,1,7]))
