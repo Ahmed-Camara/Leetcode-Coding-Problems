628f340e77abfb163b97a23219d11089a1128305
Ahmed-Camara <camarahmed123@gmail.com>
2023-06-06 16:42:52 +0100

    3. Longest Substring Without Repeating Characters

---
+++ SLIDING WINDOW/medium/Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        length = len(s)
+
+        if length == 0 or length == 1:
+            return length
+        
+        maxCount = -1
+        window = ""
+        length = len(s)
+        for i in range(length):
+            
+            strs = s[i]
+
+            if strs in window:
+                window = window[window.index(strs)+1:]
+            window = window + strs
+            maxCount = max(maxCount,len(window))
+        return maxCount
+    
+print(Solution().lengthOfLongestSubstring("abcabcbb"))
+print(Solution().lengthOfLongestSubstring("pwwkew"))
+print(Solution().lengthOfLongestSubstring("aab"))
+print(Solution().lengthOfLongestSubstring("bbbbb"))
