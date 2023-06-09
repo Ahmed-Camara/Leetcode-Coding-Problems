9e187e08a92ac2e45d36a2f593d45621f3c0ee1d
Ahmed-Camara <camarahmed123@gmail.com>
2023-06-09 11:11:37 +0100

    220. Contains Duplicate III

---
+++ SLIDING WINDOW/hard/ContainsDuplicate_III.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsNearbyAlmostDuplicate(self, nums, indexDiff, valueDiff):
+        """
+        :type nums: List[int]
+        :type indexDiff: int
+        :type valueDiff: int
+        :rtype: bool
+        """
+
+        return False
+
+print(Solution().containsNearbyAlmostDuplicate([1,2,3,1],3,0))
+print(Solution().containsNearbyAlmostDuplicate([1,5,9,1,5,9],2,3))
