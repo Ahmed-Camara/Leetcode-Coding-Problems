b934aad469629896428cab90e0063d1a0f1cab1c
Ahmed-Camara <camarahmed123@gmail.com>
2023-06-14 19:54:23 +0100

    643. Maximum Average Subarray I

--- SLIDING WINDOW/easy/Maximum Average Subarray I/MaximumAverageSubarray_I.py
+++ SLIDING WINDOW/easy/Maximum Average Subarray I/MaximumAverageSubarray_I.py
@@ -1,29 +1,22 @@
-class Solution(object):
-    def findMaxAverage(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: float
-        """
+from typing import List
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
         length = len(nums)
         if length == 1:
             return nums[0]
-        
-        
-        maxAverage = -1
-        sumNums = 0
-        j = 0
-        i = 0
+        maxAverage = float('-inf')
+        sums = 0
+        i,j = 0,0
         
         for i in range(length):
-            sumNums = sumNums + nums[i]
-            if i - j + 1  == k:
-                avg = sumNums / k
+            sums += nums[i]
+            if i - j + 1 == k:
+                avg = sums / k
                 maxAverage = max(maxAverage,avg)
-                sumNums = sumNums - nums[j]
+                sums -= nums[j]
                 j = j + 1
-            
+                
         return maxAverage
-
 print(Solution().findMaxAverage([1,12,-5,-6,50,3],4))
 print(Solution().findMaxAverage([5],1))
