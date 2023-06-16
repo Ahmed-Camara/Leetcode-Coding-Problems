21c6126bc33a401fefe4b858ef3aaa46d7b7e6c6
Ahmed-Camara <camarahmed123@gmail.com>
2023-06-16 11:14:45 +0100

    1763. Longest Nice Substring

--- SLIDING WINDOW/easy/Longest Nice Substring/LongestNiceSubstring.py
+++ SLIDING WINDOW/easy/Longest Nice Substring/LongestNiceSubstring.py
@@ -2,3 +2,8 @@ class Solution:
     def longestNiceSubstring(self, s: str) -> str:
         longestSubstring = ""
         return longestSubstring
+
+
+print(Solution().longestNiceSubstring("YazaAay"))
+print(Solution().longestNiceSubstring("Bb"))
+print(Solution().longestNiceSubstring("c"))
